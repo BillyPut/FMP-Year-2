b9e8025a697244fba5a9c0c7c99111c7aa80b797
BillyPut <90899464+BillyPut@users.noreply.github.com>
2023-05-11 16:08:53 +0100

    Settings
    - Resolution, vsync, shadows some quality overall
    - Fixed guns infinite reload

--- Assets/Scripts/UiScripts/SettingsScript.cs
+++ Assets/Scripts/UiScripts/SettingsScript.cs
@@ -9,26 +9,220 @@ using UnityEngine.Rendering.HighDefinition;
 public class SettingsScript : MonoBehaviour
 {
     public Slider brightness;
+
     public Volume volume;
     public VolumeProfile volumeProfile;
+
+    public Toggle fullscreenToggle, vSyncToggle, shadowToggle;
+    //bools for Fullscreen, Vsync and Shadows
+    private bool isOnT, isOnV, isOnS;
+
+    public Camera cam;
+    private HDAdditionalCameraData hdCam;
+
+    public TMP_Dropdown resolutionDropdown;
+    Resolution[] resolutions;
+    //public int currentResolutionIndex;
+
+    public Button hfQButton, pQButton;
 
     // Start is called before the first frame update
     void Start()
     {
+        hdCam = cam.GetComponent<HDAdditionalCameraData>();
+
+        resolutions = Screen.resolutions;
+        resolutionDropdown.ClearOptions();
+
+        List<string> options = new List<string>();
+
+        //currentResolutionIndex = 0;
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].ToString();
+            options.Add(option);
+
+            /*if (resolutions[i].width == Screen.width && resolutions[i].height == Screen.height)
+            {
+                currentResolutionIndex = i;             
+            }*/
+        }
+        
+        resolutionDropdown.AddOptions(options);
+        //resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+
+        if (PlayerPrefs.HasKey("Resolution") == true)
+        {
+            resolutionDropdown.value = PlayerPrefs.GetInt("Resolution");
+        }
+        else
+        {
+            PlayerPrefs.SetInt("Resolution", options.Count);
+            resolutionDropdown.value = PlayerPrefs.GetInt("Resolution");
+        }
+        
         volumeProfile = volume.sharedProfile;
+
+        if (PlayerPrefs.HasKey("Quality") == true)
+        {
+            QualitySettings.SetQualityLevel(PlayerPrefs.GetInt("Quality"));
+        }
+        else
+        {
+            PlayerPrefs.SetInt("Quality", 0);
+            QualitySettings.SetQualityLevel(PlayerPrefs.GetInt("Quality"));
+        }
+
+        if (PlayerPrefs.GetInt("IsOnT") == 1)
+        {
+            isOnT = true;
+        }
+        else
+        {
+            isOnT = false;   
+        }
+
+        fullscreenToggle.isOn = isOnT;
+
+        if (PlayerPrefs.GetInt("IsOnV") == 1)
+        {
+            isOnV = true;
+        }
+        else
+        {
+            isOnV = false;
+        }
+
+        vSyncToggle.isOn = isOnV;
+
+        if (PlayerPrefs.GetInt("IsOnS") == 1)
+        {
+            isOnS = true;
+        }
+        else
+        {
+            isOnV = false;
+        }
+
+        shadowToggle.isOn = isOnS;
+
+        if (PlayerPrefs.HasKey("bright") == true)
+        {
+            brightness.value = PlayerPrefs.GetFloat("bright");
+        }
+        else
+        {
+            PlayerPrefs.SetFloat("bright", 0f);
+            brightness.value = PlayerPrefs.GetFloat("bright");
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(!volumeProfile.TryGet<ColorAdjustments>(out var exposure))
+        SettingPlayerPrefs();
+        //Enable camera override for shadow maps
+        hdCam.renderingPathCustomFrameSettingsOverrideMask.mask[(int)FrameSettingsField.ShadowMaps] = true;
+    }
+
+    public void SettingPlayerPrefs()
+    {
+        if (fullscreenToggle.isOn == true)
+        {
+            PlayerPrefs.SetInt("IsOnT", 1);
+        }
+        else
+        {
+            PlayerPrefs.SetInt("IsOnT", 0);
+        }
+
+        if (vSyncToggle.isOn == true)
+        {
+            PlayerPrefs.SetInt("IsOnV", 1);
+        }
+        else
+        {
+            PlayerPrefs.SetInt("IsOnV", 0);
+        }
+
+        if (shadowToggle.isOn == true)
+        {
+            PlayerPrefs.SetInt("IsOnS", 1);
+        }
+        else
+        {
+            PlayerPrefs.SetInt("IsOnS", 0);
+        }
+    }
+
+    public void SetHighFidelity()
+    {
+        QualitySettings.SetQualityLevel(0);
+        PlayerPrefs.SetInt("Quality", 0);
+    }
+
+    public void SetPerformant()
+    {
+        QualitySettings.SetQualityLevel(2);
+        PlayerPrefs.SetInt("Quality", 2);
+    }
+
+    public void SetResolution(int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+        PlayerPrefs.SetInt("Resolution", resolutionIndex);
+        Debug.Log(resolutionIndex);
+    }
+
+    public void SetFullscreen(bool isFullscreen)
+    {
+        if (isFullscreen == true)
+        {
+            Screen.fullScreenMode = FullScreenMode.FullScreenWindow;
+        }
+        else
+        {
+            Screen.fullScreenMode = FullScreenMode.Windowed;
+        }
+    }
+
+    public void SetVsync(bool isVsync)
+    {
+        if (isVsync == true)
+        {
+            QualitySettings.vSyncCount = 1;
+        }
+        else
+        {
+            Application.targetFrameRate = 60;
+            QualitySettings.vSyncCount = 0;
+        }
+    }
+
+    public void SetShadows(bool isShadows)
+    {
+        //Turns the shadow override on and off
+        if (isShadows == true)
+        {
+            hdCam.renderingPathCustomFrameSettings.SetEnabled(FrameSettingsField.ShadowMaps, true);
+        }
+        else
+        {
+            hdCam.renderingPathCustomFrameSettings.SetEnabled(FrameSettingsField.ShadowMaps, false);
+        }
+    }
+
+    public void SetBrightness (float brightnessLevel)
+    {
+        if (!volumeProfile.TryGet<ColorAdjustments>(out var exposure))
         {
             exposure = volumeProfile.Add<ColorAdjustments>(false);
         }
 
-        exposure.postExposure.value = brightness.value;
-
-       
-
+        exposure.postExposure.value = brightnessLevel;
+        PlayerPrefs.SetFloat("bright", brightness.value);
     }
+
 }
--- Assets/Scripts/WeaponSystem.cs
+++ Assets/Scripts/WeaponSystem.cs
@@ -30,44 +30,51 @@ public class WeaponSystem : MonoBehaviour
 
         Debug.DrawRay(cam.transform.position, cam.transform.forward, Color.red);
 
-
-        if (Input.GetMouseButton(0) && firingTime <= 0 && gunData.ammo > 0 && gunData.reloading == false)
+        if (Input.GetMouseButtonDown(0) && gunData.reloading == false)
         {
-            RaycastHit laserHit;
-            if (Physics.Raycast(cam.transform.position, cam.transform.forward, out laserHit, target))
+            if (firingTime <= 0 && gunData.ammo > 0)
             {
-                Debug.Log(laserHit.collider.name);
-                
-                if (laserHit.collider.tag == "Enemy")
+                RaycastHit laserHit;
+                if (Physics.Raycast(cam.transform.position, cam.transform.forward, out laserHit, target))
                 {
-                    laserHit.transform.SendMessageUpwards("TakeDamage", gunData.damage);
+                    Debug.Log(laserHit.collider.name);
+
+                    if (laserHit.collider.tag == "Enemy")
+                    {
+                        laserHit.transform.SendMessageUpwards("TakeDamage", gunData.damage);
+                    }
+
+                    if (gunData.name == "Blaster")
+                    {
+                        Instantiate(explosion, laserHit.point, laserHit.transform.rotation);
+                    }
+                    else
+                    {
+                        Quaternion instDir = Quaternion.FromToRotation(laserHit.normal, Vector3.forward);
+                        Instantiate(particleHit, laserHit.point, instDir);
+                    }
+
+
                 }
 
-                if (gunData.name == "Blaster")
-                {
-                    Instantiate(explosion, laserHit.point, laserHit.transform.rotation);
-                }
-                else
-                {
-                    Quaternion instDir = Quaternion.FromToRotation(laserHit.normal, Vector3.forward);
-                    Instantiate(particleHit, laserHit.point, instDir);
-                }
-
-
+                gunData.ammo -= 1;
+                firingTime = gunData.fireRate;
             }
-
-            gunData.ammo -= 1;
-            firingTime = gunData.fireRate;
-
-
+            if (gunData.ammo <= 0 && this.gameObject.activeSelf)
+            {
+                StartCoroutine(Reload());
+            }
         }
 
-        if (Input.GetKeyDown("r") && gunData.reloading == false && gunData.ammo != gunData.magsize && this.gameObject.activeSelf) 
+        
+
+        if (Input.GetKeyDown("r") && gunData.reloading == false && gunData.ammo != gunData.magsize && this.gameObject.activeSelf)
         {
-            StartCoroutine(Reload());
+            if (gunData.overallAmmo > 0)
+            {
+                StartCoroutine(Reload());
+            }
         }
-
-
     }
 
     private IEnumerator Reload()
@@ -76,13 +83,19 @@ public class WeaponSystem : MonoBehaviour
 
         yield return new WaitForSeconds(gunData.reloadTime);
 
-        ammoDecrease = gunData.magsize - gunData.ammo;
-        //gunData.overallAmmo -= ammoDecrease;
-
-        gunData.ammo = gunData.magsize;
+        if (gunData.ammo + gunData.overallAmmo >= gunData.magsize)
+        {
+            ammoDecrease = gunData.magsize - gunData.ammo;
+            //gunData.overallAmmo -= ammoDecrease;
+            gunData.ammo = gunData.magsize;
+        }
+        else
+        {
+            gunData.ammo += gunData.overallAmmo;
+            gunData.overallAmmo = 0;
+        }
 
         gunData.reloading = false;
-
 
     }
 
